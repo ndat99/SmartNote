5da19e0feb4da9e6f21fccafef8fb2846ba015cb
TIEN <ndatien191@gmail.com>
2026-02-27 17:56:57 +0700

    feat: thêm file migration và các model ban đầu; cập nhật file .gitignore và đăng ký các model vào trang quản trị admin

--- notes/admin.py
+++ notes/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Category, Note, ChecklistItem, NoteImage
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(Note)
+admin.site.register(ChecklistItem)
+admin.site.register(NoteImage)
