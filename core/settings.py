44d2cdb934aa3e207381e93f5e56e0c9ab9771ff
Hoang Phuc <phucqaqs@gmail.com>
2026-02-28 23:02:57 +0700

    DEBUG = True

--- core/settings.py
+++ core/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 ALLOWED_HOSTS = ['*']
-
+DEBUG = True
 
 INSTALLED_APPS = [
     'django.contrib.admin',
